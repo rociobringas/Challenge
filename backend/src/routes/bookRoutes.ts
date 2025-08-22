ef7b0b75f309aaa4940c1abe2b1352703717e601
Rocio Bringas <rocio@MacBook-Air-3.local>
2025-08-22 18:18:34 -0300

    routes

---
+++ backend/src/routes/bookRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { BookController } from "../modules/book/controller/bookController";
+
+const router = Router();
+const controller = new BookController();
+
+router.get("/", controller.getAll.bind(controller));
+router.get("/:id", controller.getById.bind(controller));
+router.post("/", controller.create.bind(controller));
+router.delete("/:id", controller.delete.bind(controller));
+
+export default router;
---
+++ backend/src/routes/libraryRoots.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {LibraryController} from "../modules/library/controller/libraryController";
+
+const router = Router();
+const controller = new LibraryController();
+
+router.get("/", controller.getAll.bind(controller));
+router.get("/:id", controller.getById.bind(controller));
+router.post("/", controller.add.bind(controller));
+router.delete("/:id", controller.remove.bind(controller));
+
+export default router;
